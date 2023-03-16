ba3671f487879bf9285a91a731215722930331e8
Alexander V <non7sense@ya.ru>
2023-03-17 02:50:48 +0300

    Сreated the primary layout of the playing field

--- TicTacToeApp/TicTacToeApp/GameModule/GameView.swift
+++ TicTacToeApp/TicTacToeApp/GameModule/GameView.swift
@@ -7,10 +7,65 @@
 
 import SwiftUI
 
-struct GameView: View {
-    var body: some View {
-        Text("TicTacToe")
+enum Player {
+    case cross, zero
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .cross ? "cross" : "zero"
     }
+}
+
+
+struct GameView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isCrossTurn = true
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            ZStack {
+                GridView(frameGrid: (width: geometry.size.width, height: geometry.size.width), indentLines: 8, thickness: 2, opacity: 0.2)
+                
+                VStack {
+                    Spacer()
+                    LazyVGrid(columns: columns, spacing: 17) {
+                        ForEach(0..<9) { i in
+                            ZStack {
+                                
+//                                CrossView(angleForce: 0.12)
+//                                    .frame(width: geometry.size.width/3 - 15,
+//                                           height: geometry.size.width/3 - 15)
+                                
+//                                Circle()
+//                                    .strokeBorder(lineWidth: 9)
+//                                    .frame(width: geometry.size.width/3 - 15,
+//                                           height: geometry.size.width/3 - 15)
+                            }
+                            .onTapGesture {
+                                moves[i] = Move(player: isCrossTurn ? .cross : .zero, boardIndex: i)
+                                isCrossTurn.toggle()
+                            }
+                        }
+                    }
+                    Spacer()
+                } .padding(3)
+                
+            }
+            
+        }
+ 
+    }
+    
 }
 
 struct GameView_Previews: PreviewProvider {
